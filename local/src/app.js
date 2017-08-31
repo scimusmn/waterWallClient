976fc8d8e56995e53f5597ecb208ad69dd44980b
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2017-08-31 16:26:10 -0500

    test pulse

--- local/src/app.js
+++ local/src/app.js
@@ -1,10 +1,28 @@
 'use strict';
 
-obtain(['./src/server/express.js'], (hw)=> {
+obtain(['rpio'], (hw)=> {
   exports.app = {};
+
+  var outs = [2, 3, 4, 17, 18, 27, 22, 23, 24, 10, 9, 25, 11, 8, 7, 0, 1, 5, 6, 12, 13, 19, 16, 26];
+
+  rpio.init({ mapping: 'gpio' });
+
+  var count = 0;
+
+  var activateNext = ()=> {
+    rpio.write(outs[count], rpio.LOW);
+    count = (count + 1) % outs.length;
+    rpio.write(outs[count], rpio.HIGH);
+  };
 
   exports.app.start = ()=> {
     console.log('started');
+
+    for (var i = 0; i < outs.length; i++) {
+      rpio.open(outs[i], rpio.OUTPUT, rpio.LOW);
+    }
+
+    setInterval(activateNext, 500);
 
     document.onkeypress = (e)=> {
       if (e.key == ' ') console.log('Space pressed');
--- piFig/install.sh
+++ piFig/install.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
 
-echo "Clone the application"
+
+echo "\nInstalling node:"
+
+sudo apt-get install --no-install-recommends xserver-xorg xinit xserver-xorg-video-fbdev xserver-xorg-video-fbturbo libxss1 libgconf-2-4 libnss3 git nodejs libgtk2.0-0 libxtst6
+
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+
+echo "\nClone the application"
 
 git clone https://github.com/scimusmn/waterWallClient.git
 
 cd waterWallClient
 
-echo "Init the submodules:"
+echo "\nInit the submodules:"
 
 git submodule init
+
 git submodule update
 
-echo "Adding Adafruit Declarations and installing node:"
-
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-
-sudo apt-get install node
-
-#echo "Creating Device Tree Blob for halt signal on GPIO25:"
-
-#sudo dtc -I dts -O dtb -o /boot/dt-blob.bin my-blob.dts
-
-echo "Installing dependencies for application:"
+echo "\nInstalling dependencies for application:"
 
 npm i
+
+echo "\nConfiguring"
+
+cd piFig
+
+sudo node installPi.js
