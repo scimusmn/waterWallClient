28a3b23e7645c68319072cff217254e85ccbc20c
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2017-09-05 10:20:21 -0500

    added raster printing

--- local/src/wallControl.js
+++ local/src/wallControl.js
@@ -38,7 +38,7 @@ obtain(['fs', 'rpio'], (fs, rpio)=> {
           setTimeout(()=> {
             out.state = rpio.LOW;
             rpio.write(out.pin, rpio.LOW);
-          });
+          }, _this.pixel.height);
         }, delay);
       }
     };
