3595210277e541473dc45342b4831e2612932b7e
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2017-09-05 09:57:06 -0500

    added raster printing

--- local/src/app.js
+++ local/src/app.js
@@ -1,33 +1,35 @@
 'use strict';
 
-obtain(['rpio'], (hw)=> {
+obtain(['./src/wallControl.js'], ({ valves })=> {
   exports.app = {};
 
-  var outs = [2, 3, 4, 17, 18, 27, 22, 23, 24, 10, 9, 25, 11, 8, 7, 0, 1, 5, 6, 12, 13, 19, 16, 26];
-
-  rpio.init({ mapping: 'gpio' });
+  var test = [[1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0],
+              [1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0],
+              [0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1],
+              [0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1],
+              [1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0],
+              [1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0],
+              [0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1],
+              [0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1],
+              [1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0],
+              [1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0],
+              [0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1],
+              [0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1],
+  ];
 
   var count = 0;
 
-  var activateNext = ()=> {
-    rpio.write(outs[count], rpio.LOW);
-    count = (count + 1) % outs.length;
-    rpio.write(outs[count], rpio.HIGH);
-  };
-
   exports.app.start = ()=> {
     console.log('started');
-
-    for (var i = 0; i < outs.length; i++) {
-      rpio.open(outs[i], rpio.OUTPUT, rpio.LOW);
-    }
-
-    setInterval(activateNext, 500);
 
     document.onkeyup = (e)=> {
       var electron = require('electron');
       if (e.which == 27) electron.remote.process.exit();
     };
+
+    setInterval(()=> {
+      valves.drawRaster(test, Date.now());
+    }, (test.length + 1) * valves.pixel.height);
 
   };
 
---
+++ local/src/wallControl.js
@@ -0,0 +1,68 @@
+obtain(['fs', 'rpio'], (fs, rpio)=> {
+  //exports.valves = {};
+
+  var outs = [2, 3, 4, 17, 18, 27, 22, 23, 24, 10, 9, 25, 11, 8, 7, 0, 1, 5, 6, 12, 13, 19, 16, 26];
+
+  var valves = function() {
+    var _this = this;
+
+    this.timeOffset = 0;
+    this.pixel = { width: 2, height: 50 };
+
+    rpio.init({ mapping: 'gpio' });
+
+    for (var i = 0; i < outs.length; i++) {
+      let num = outs[i];
+      outs[i] = { pin: num, state: rpio.LOW, scheduled: 0 };
+      rpio.open(outs[i].pin, rpio.OUTPUT, rpio.LOW);
+    }
+
+    _this.synchronize = ()=> {
+      var data = { time: Date.now() };
+      post('http://192.168.0.1/timeSync', data).then((resp)=> {
+        this.timeOffset = (2 * resp.serverTime - (data.time + Date.now())) / 2;
+      }, (error)=> {
+        console.log('Could not sync with server');
+        this.timeOffset = 0;
+      });
+    };
+
+    _this.set = (num, length, stamp)=> {
+      let out = outs[num];
+      let delay = Math.max(0, stamp - Date.now());
+      if (!out.state && stamp > out.scheduled) {
+        if (stamp > out.scheduled) out.scheduled = stamp + length;
+        setTimeout(()=> {
+          out.state = rpio.HIGH;
+          rpio.write(out.pin, out.state);
+          setTimeout(()=> {
+            out.state = rpio.LOW;
+            rpio.write(out.pin, out.state);
+          });
+        }, delay);
+      }
+    };
+
+    _this.rasterRow = (data, stamp)=> {
+      if (data.length == outs.length / _this.pixel.width) {
+        for (var i = 0; i < data.length; i++) {
+          if (data[i]) {
+            _this.set(2 * i, _this.pixel.height, stamp);
+            _this.set(2 * i + 1, _this.pixel.height, stamp);
+          }
+        }
+      }
+    };
+
+    _this.drawRaster = (data, stamp)=> {
+      for (var i = 0; i < data.length; i++) {
+        _this.rasterRow(data, stamp + (data.length - (i + 1)) * _this.pixel.height);
+      }
+    };
+
+  };
+
+  exports.valves = new valves();
+
+  provide(exports);
+});
